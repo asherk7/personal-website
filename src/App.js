29b630a9efb5fcec6711c9d2dd15881f5bd4b142
asherk7 <masherk03@hotmail.com>
2023-07-05 19:52:26 -0400

    Added header file, edited app and index files to adjust

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
@@ -9,6 +10,7 @@ import Footer from './components/footer/Footer';
 function App() {
   return (
     <div>
+      <Header />
       <Nav />
       <About />
       <Skills />
