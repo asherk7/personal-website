3fac87873751b86d649a210a7e5fd9ebbdcbb370
asherk7 <masherk03@hotmail.com>
2023-07-05 20:14:39 -0400

    added picture

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './header.css'
+import me from '../../resources/asher_khan.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header-container'>
+        <div className="me">
+          <img src={me} alt="Asher Khan" />
+        </div>
+
+        <h1>Asher Khan</h1>
+        <h5 className="text-light">Software Engineering student - aspiring Machine Learning Engineer</h5>
+
+      </div>
+    </header>
   )
 }
 
