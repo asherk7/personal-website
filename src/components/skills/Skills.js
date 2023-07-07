ad407607b1b48da2614550e2632968a82589d5f9
asherk7 <masherk03@hotmail.com>
2023-07-07 16:00:11 -0400

    added pictures for skills, and updated skill list

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -6,6 +6,67 @@ const Skills = () => {
     <section id="skills">
       <div className="container">
         <h1>Skills</h1>
+
+        <h2>programming languages</h2>
+        <ul>
+          <li>Python</li>
+          <li>Java</li>
+          <li>C</li>
+          <li>Bash</li>
+          <li>MATLAB</li>
+        </ul>
+
+        <h2>machine learning/data science</h2>
+        <ul>
+          <li>TensorFlow</li>
+          <li>PyTorch</li>
+          <li>Keras</li>
+          <li>SciPy</li>
+          <li>SciKit-Learn</li>
+          <li>NumPy</li>
+          <li>Pandas</li>
+          <li>Matplotlib</li> 
+          <li>Seaborn</li>
+        </ul>
+
+        <h2>data bases</h2>
+        <ul>
+          <li>MySQL</li>
+          <li>MongoDB</li>
+          <li>SQLite</li>
+        </ul>
+
+        <h2>web development</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+          <li>React</li>
+          <li>Node.js</li>
+          <li>Express</li>
+        </ul>
+
+        <h2>frameworks</h2>
+        <ul>
+          <li>Bootstrap</li>
+          <li>Django</li>
+          <li>Flask</li>
+          <li>Angular</li>
+        </ul>
+
+        <h2>devops</h2>
+        <ul>
+          <li>Git</li>
+          <li>JUnit</li>
+          <li>Jira</li>
+          <li>Docker</li>
+          <li>Jenkins</li>
+          <li>AWS</li>
+          <li>SonarQube</li>
+          <li>Maven</li>
+        </ul>
+
       </div>
     </section>
   )
