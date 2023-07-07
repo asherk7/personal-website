3629990a5ed2ece814f084bbaa3041b757404383
asherk7 <masherk03@hotmail.com>
2023-07-07 01:07:20 -0400

    finished navbar

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {RiContactsFill} from 'react-icons/ri'
@@ -7,13 +7,19 @@ import {VscTools} from 'react-icons/vsc'
 import './nav.css'
 
 const Nav = () => {
+  const [nav, setNav] = useState("#home")
+
+  async function changeNav(e) {
+    setNav(e.currentTarget.getAttribute("href"));
+  }
+
   return (
     <nav>
-      <a href="#home"><AiOutlineHome /></a>
-      <a href="#about"><AiOutlineUser /></a>
-      <a href="#skills"><VscTools /></a>
-      <a href="#projects"><LiaBrainSolid /></a>
-      <a href="#contact"><RiContactsFill /></a>
+      <a onClick={changeNav} className={nav === "#home" ? "currentview" : ""} href="#home"><AiOutlineHome /></a>
+      <a onClick={changeNav} className={nav === "#about" ? "currentview" : ""} href="#about"><AiOutlineUser /></a>
+      <a onClick={changeNav} className={nav === "#skills" ? "currentview" : ""} href="#skills"><VscTools /></a>
+      <a onClick={changeNav} className={nav === "#projects" ? "currentview" : ""} href="#projects"><LiaBrainSolid /></a>
+      <a onClick={changeNav} className={nav === "#contact" ? "currentview" : ""} href="#contact"><RiContactsFill /></a>
     </nav>
   )
 }
