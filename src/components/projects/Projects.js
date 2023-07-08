99c840667cb7630b878cc3441970dd8f53c11a48
asherk7 <masherk03@hotmail.com>
2023-07-08 18:47:45 -0400

    added responsive design for the project section

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import NFL from '../../resources/projects/NFL.jpg'
+import nfl from '../../resources/projects/nfl.jpg'
 import island from '../../resources/projects/island.jpg'
 import jaguars from '../../resources/projects/jaguars.jpg'
 import './projects.css'
@@ -8,28 +8,28 @@ const Projects = () => {
   return (
     <section id="projects">
       <h1>Projects</h1>
-      <h5>My Recent Work, More to Come</h5>
+      <h2>My Recent Work, More to Come</h2>
 
       <div className="container projects-container">
         <article className="project">
-          <div className="project-image"></div>
+          <div className="project-image">
+            <img src={nfl} alt="NFL Record Predictor" />
+          </div>
           <h3>NFL Record Predictor</h3>
           <a href="https://github.com/asherk7/NFL-Record-Predictor" target="_blank" rel="noopener noreferrer" className="btn">Github</a>
           <a href="https://nflrecordpredictor.pythonanywhere.com/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">Live Demo</a>
         </article>
-      </div>
-
-      <div className="container projects-container">
         <article className="project">
-          <div className="project-image"></div>
+          <div className="project-image">
+            <img src={island} alt="Island Generator" />
+          </div>
           <h3>Island Generator</h3>
           <a href="https://github.com/asherk7/Island-Generator" target="_blank" rel="noopener noreferrer" className="btn">Github</a>
         </article>
-      </div>
-
-      <div className="container projects-container">
         <article className="project">
-          <div className="project-image"></div>
+          <div className="project-image">
+            <img src={jaguars} alt="Jaguars Webpage" />
+          </div>
           <h3>Jaguars Webpage</h3>
           <a href="https://github.com/asherk7/jaguars-webpage" target="_blank" rel="noopener noreferrer" className="btn">Github</a>
           <a href="https://asherk7.github.io/jaguars-webpage/m" target="_blank" rel="noopener noreferrer" className="btn btn-primary">Live Demo</a>
