d2ae250322cf911d747372195590b51b925f2d7f
asherk7 <masherk03@hotmail.com>
2023-07-08 20:20:54 -0400

    added a data file for the projects, and rendered each seperately through another project file

---
+++ src/components/projects/Project.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './projects.css'
+
+const Project = (props) => {
+  let projectdata = props.data
+
+  return (
+    <article key={projectdata.id} className="project">
+      <div className="project-image">
+        <img src={projectdata.image} alt={projectdata.title} />
+      </div>
+      <h3>{projectdata.title}</h3>
+      <a href={projectdata.github} target="_blank" rel="noopener noreferrer" className="btn">Github</a>
+      {projectdata.demo !== "" ? 
+      <a href={projectdata.demo} target="_blank" rel="noopener noreferrer" className="btn btn-primary">Live Demo</a>
+      : ""}
+    </article>
+  )
+}
+
+export default Project
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,70 +1,71 @@
-import React from 'react'
-import nfl from '../../resources/projects/nfl.jpg'
-import island from '../../resources/projects/island.jpg'
-import jaguars from '../../resources/projects/jaguars.jpg'
-import workout from '../../resources/projects/workout.jpg'
-import tbd from '../../resources/programming.png'
-import './projects.css'
+import React from 'react';
+import Project from './Project.js';
+import nfl from '../../resources/projects/nfl.jpg';
+import island from '../../resources/projects/island.jpg';
+import jaguars from '../../resources/projects/jaguars.jpg';
+import workout from '../../resources/projects/workout.jpg';
+import tbd from '../../resources/programming.png';
+import "./projects.css"
 
 const Projects = () => {
-  return (
-    <section id="projects">
-      <h1>Projects</h1>
-      <h2>My Recent Work, More to Come</h2>
+    const data = [
+        {
+            id: 1,
+            image: nfl,
+            title: "NFL Record Predictor",
+            github: "https://github.com/asherk7/NFL-Record-Predictor",
+            demo: "https://nflrecordpredictor.pythonanywhere.com/"
+        },
+        {
+            id: 2,
+            image: island,
+            title: "Island Generator",
+            github: "https://github.com/asherk7/Island-Generator",
+            demo: ""
+        },
+        {
+            id: 3,
+            image: jaguars,
+            title: "Jaguars Webpage",
+            github: "https://github.com/asherk7/jaguars-webpage",
+            demo: "https://asherk7.github.io/jaguars-webpage/"
+        },
+        {
+            id: 4,
+            image: workout,
+            title: "Workout Tracker",
+            github: "https://github.com/asherk7/Workout-Tracker",
+            demo: ""
+        },
+        {
+            id: 5,
+            image: tbd,
+            title: "Final Project for Odin Project, Coming Soon",
+            github: "https://github.com/asherk7",
+            demo: "https://github.com/asherk7"
+        },
+        {
+            id: 6,
+            image: tbd,
+            title: "Full-Stack Machine Learning Project, Coming Soon",
+            github: "https://github.com/asherk7",
+            demo: "https://github.com/asherk7"
+        }
+    ]
 
-      <div className="container projects-container">
-        <article className="project">
-          <div className="project-image">
-            <img src={nfl} alt="NFL Record Predictor" />
-          </div>
-          <h3>NFL Record Predictor</h3>
-          <a href="https://github.com/asherk7/NFL-Record-Predictor" target="_blank" rel="noopener noreferrer" className="btn">Github</a>
-          <a href="https://nflrecordpredictor.pythonanywhere.com/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">Live Demo</a>
-        </article>
-
-        <article className="project">
-          <div className="project-image">
-            <img src={island} alt="Island Generator" />
-          </div>
-          <h3>Island Generator</h3>
-          <a href="https://github.com/asherk7/Island-Generator" target="_blank" rel="noopener noreferrer" className="btn">Github</a>
-        </article>
-
-        <article className="project">
-          <div className="project-image">
-            <img src={jaguars} alt="Jaguars Webpage" />
-          </div>
-          <h3>Jaguars Webpage</h3>
-          <a href="https://github.com/asherk7/jaguars-webpage" target="_blank" rel="noopener noreferrer" className="btn">Github</a>
-          <a href="https://asherk7.github.io/jaguars-webpage/m" target="_blank" rel="noopener noreferrer" className="btn btn-primary">Live Demo</a>
-        </article>
-
-        <article className="project">
-          <div className="project-image">
-            <img src={workout} alt="Workout Tracker" />
-          </div>
-          <h3>Workout Tracker</h3>
-          <a href="https://github.com/asherk7/Workout-Tracker" target="_blank" rel="noopener noreferrer" className="btn">Github</a>
-        </article>
-
-        <article className="project">
-          <div className="project-image">
-            <img src={tbd} alt="To Be Decided" />
-          </div>
-          <h3>Final Project for Odin Project, Coming Soon</h3>
-          <a href="https://github.com/asherk7" target="_blank" rel="noopener noreferrer" className="btn">Github</a>
-        </article>
-
-        <article className="project">
-          <div className="project-image">
-            <img src={tbd} alt="To Be Decided" />
-          </div>
-          <h3>Full-Stack Machine Learning Project, Coming Soon</h3>
-          <a href="https://github.com/asherk7" target="_blank" rel="noopener noreferrer" className="btn">Github</a>
-        </article>
-      </div>
-    </section>
-  )
+    return (
+        <section id="projects">
+            <h1>Projects</h1>
+            <h2>My Recent Work, More to Come</h2>
+            <div className="container projects-container">
+                {data.map((project) => {
+                    return (
+                        <Project data={project} />
+                    )
+                })}
+            </div>
+        </section>
+    );
 }
 
 export default Projects
