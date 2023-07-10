7aeaad18347a3ec043ceb99cd0c78c43f1964f6a
asherk7 <masherk03@hotmail.com>
2023-07-10 17:54:14 -0400

    fixed website description

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -12,7 +12,7 @@ const About = () => {
         </div>
         <div className="aboutme">
           <h2>McMaster '26 - B.Eng - Software Engineer</h2>
-          <h4> I am a student at McMaster University, class of 2026. I am passionate about the field of software engineering, particularly in the areas of machine learning, math, statistics, software design, AI, and data science.</h4>
+          <h4>I am a student at McMaster University, class of 2026. I am passionate about the field of software engineering, particularly in the areas of machine learning, math, statistics, software design, AI, and data science.</h4>
           <h4>I'm captivated by the possibilities of machine learning and its ability to make intelligent decisions. I enjoy exploring algorithms and techniques that power machine learning models to solve real-world problems.</h4>
           <h4>Currently, I'm focused on learning more about machine learning. I stay updated with the latest developments and undertake practical projects to expand my expertise in this rapidly evolving field.</h4>
         </div>
